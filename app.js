2ea39992d1dc1fa76e511129498f40ed68075fcf
rishikgoud <goudrishik2006@gmail.com>
2024-04-20 22:28:08 +0530

    added

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 let inputtext = document.querySelector(".inputtext");
 let qrimage = document.querySelector(".qrimage");
 let qrbox = document.querySelector(".image");
+let subcontain = document.querySelector(".subcontainer");
+let p = document.createElement("span");
+// let delimage = document.querySelector(".inputimg");
 
 function generateqrimg(){
     if(inputtext.value.length > 0){
         qrimage.src = "https://api.qrserver.com/v1/create-qr-code/?&data=" + inputtext.value;
         qrbox.classList.add("show-img");
+        p.innerHTML= "\u00d7";
+        p.classList.add("showimg");
+        subcontain.appendChild(p);
+        p.addEventListener("click",deleteqr);
     }
     else{
         inputtext.classList.add("error");
@@ -14,3 +21,11 @@ function generateqrimg(){
         }, 1000);
     }
 }
+function deleteqr(){
+    p.innerHTML = '';
+    inputtext.value = '';
+    qrimage.src = '';
+    qrbox.classList.remove("show-img");
+
+}
+
